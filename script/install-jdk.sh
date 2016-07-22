c918977742a00a753270e963f6d4ce34a3dbe15c
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-22 21:12:03 +0700

    update install jdk script

--- script/install-jdk.sh
+++ script/install-jdk.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 cd /opt
-wget --no-cookies --no-check-certificate --header "Cookie: 
-gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; 
-oraclelicense=accept-securebackup-cookie" 
-"http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz"
+wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u91-b14/jdk-8u91-linux-x64.tar.gz"
 tar xzf jdk-8u91-linux-x64.tar.gz
 cd /opt/jdk1.8.0_91/
 alternatives --install /usr/bin/java java /opt/jdk1.8.0_91/bin/java 2
