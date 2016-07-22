4d30734948e2559d9c0743c844aeab09fbedb3ce
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-22 21:32:18 +0700

    fix script install logstash

--- script/install-logstash.sh
+++ script/install-logstash.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 wget https://raw.githubusercontent.com/akhfa/ta/master/repo/logstash.repo
-mv -f logstash.repo /etc/yum.conf.d
+mv -f logstash.repo /etc/yum.repos.d/
 rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 yum -y install logstash
