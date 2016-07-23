d5b528b3b6f6bbfb9a283cf1a6bcc5e92ba0643a
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-24 00:07:16 +0700

    add_cluster.sh: update description

--- script/add_cluster.sh
+++ script/add_cluster.sh
@@ -1,5 +1,11 @@
-# digunakan untuk membuat exchange dan queue
-# Format: ./add_cluster.sh <exchange name> <queue name>
+# Digunakan untuk membuat exchange dan queue
+# Format: 
+#	./add_cluster.sh <exchange name> <queue name>
+
+# dimana: 
+# 	exchange	=cluster
+# 	queue 		= node
+
 
 #!/bin/sh
 rabbitmqadmin -V ta -u ta -p BuatTA -H rabbitmq.akhfa.me declare exchange name=cluster1 type=fanout
