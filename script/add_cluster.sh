9fad86ad0513bb76b390afc70abb7ca7eb712e9f
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-24 00:05:49 +0700

    add add_cluster script

---
+++ script/add_cluster.sh
@@ -0,0 +1,7 @@
+# digunakan untuk membuat exchange dan queue
+# Format: ./add_cluster.sh <exchange name> <queue name>
+
+#!/bin/sh
+rabbitmqadmin -V ta -u ta -p BuatTA -H rabbitmq.akhfa.me declare exchange name=cluster1 type=fanout
+rabbitmqadmin -V ta -u ta -p BuatTA -H rabbitmq.akhfa.me declare queue name="f1" durable=true
+rabbitmqadmin -V ta -u ta -p BuatTA -H rabbitmq.akhfa.me declare binding source=cluster1 destination=f1
