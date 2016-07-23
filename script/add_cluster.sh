065e7e00a20e79f609b415573fb0c78c06b5fd04
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-24 00:41:13 +0700

    add_cluster update dari install jdk, logstash, dan add exchange + queue

--- script/add_cluster.sh
+++ script/add_cluster.sh
@@ -9,6 +9,18 @@
 #       queue           = node
 
 #!/bin/sh
+
+# Install JDK
+wget https://raw.githubusercontent.com/akhfa/ta/master/script/install-jdk.sh
+chmod +x install-jdk.sh
+./install-jdk.sh
+
+# Install Logstash
+https://raw.githubusercontent.com/akhfa/ta/master/script/install-logstash.sh
+chmod +x install-logstash.sh
+./install-logstash.sh
+
+# Install exchange (jika belum ada) dan queue untuk node ini
 wget https://raw.githubusercontent.com/akhfa/ta/master/script/add_exchange_queue.sh
 chmod +x add_exchange_queue.sh
 ./add_exchange_queue.sh $1 $2
