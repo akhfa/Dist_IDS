fc5add2820162546ebcbf515b743986b4688356c
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-26 06:19:00 +0700

    script: update to real config

--- script/add_cluster.sh
+++ script/add_cluster.sh
@@ -27,10 +27,18 @@ exchange=$5
 queue=$(hostname)
 durable=true 		# ganti false jika diinginkan
 
+# Download all script
+wget https://raw.githubusercontent.com/akhfa/ta/master/script/install-jdk.sh
+wget https://raw.githubusercontent.com/akhfa/ta/master/script/install-logstash.sh
+wget https://raw.githubusercontent.com/akhfa/ta/master/script/add_exchange_queue.sh
+
+# Get exec permission
+chmod +x install-jdk.sh
+chmod +x install-logstash.sh
+chmod +x add_exchange_queue.sh
+
 # Install JDK
 echo "Installing Oracle JDK..."
-wget https://raw.githubusercontent.com/akhfa/ta/master/script/install-jdk.sh
-chmod +x install-jdk.sh
 ./install-jdk.sh
 
 # Install rabbitmqadmin untuk menambah exchange dan queue
@@ -41,36 +49,46 @@ mv rabbitmqadmin /usr/bin
 
 # Install Logstash
 echo "Installing logstash and set logstash user as root..."
-wget https://raw.githubusercontent.com/akhfa/ta/master/script/install-logstash.sh
-chmod +x install-logstash.sh
 ./install-logstash.sh
+sed -i '/#LS_USER=logstash/a LS_USER=root' /etc/sysconfig/logstash
 
 # Install exchange (jika belum ada) dan queue untuk node ini
 echo "Adding exchange and queue"
-wget https://raw.githubusercontent.com/akhfa/ta/master/script/add_exchange_queue.sh
-chmod +x add_exchange_queue.sh
 ./add_exchange_queue.sh $host $vhost $user $password $exchange
 
 # Download config input logstash dan ubah parameter sesuai input user
-echo "Downloading logstash config"
-wget https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/01-input-rabbitmq.conf
-sed -i "s/<host>/\"$host\"/" 01-input-rabbitmq.conf
-sed -i "s/<vhost>/\"$vhost\"/" 01-input-rabbitmq.conf
-sed -i "s/<user>/\"$user\"/" 01-input-rabbitmq.conf
-sed -i "s/<password>/\"$password\"/" 01-input-rabbitmq.conf
-sed -i "s/<exchange>/\"$exchange\"/" 01-input-rabbitmq.conf
-sed -i "s/<queue>/\"$queue\"/" 01-input-rabbitmq.conf
-sed -i "s/<durable>/\"$durable\"/" 01-input-rabbitmq.conf
-mv 01-input-rabbitmq.conf /etc/logstash/conf.d/
+echo "Downloading logstash input config"
+wget https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/01-input.conf
+# input file log
+sed "s,<path>,\"/var/log/nginx/access_log\"," 01-input.conf
+
+# Input general
+sed -i "s/<host>/\"$host\"/" 01-input.conf
+sed -i "s/<vhost>/\"$vhost\"/" 01-input.conf
+sed -i "s/<user>/\"$user\"/" 01-input.conf
+sed -i "s/<password>/\"$password\"/" 01-input.conf
+sed -i "s/<durable>/\"$durable\"/" 01-input.conf
+
+
+# input untuk mendapatkan pattern dari rabbitmq
+sed -i "s/<pattern-exchange>/\"pattern-exchange\"/" 01-input.conf
+sed -i "s/<pattern-queue>/\"pattern-queue\"/" 01-input.conf
+
+# Input untuk input cluster sebagai sumber dari exec iptables
+sed -i "s/<pattern-exchange>/\"$exchange\"/" 01-input.conf
+sed -i "s/<pattern-queue>/\"$exchange\"/" 01-input.conf
+
+mv 01-input.conf /etc/logstash/conf.d/
 
 # Download config output logstash dan ubah parameter sesuai input user
-echo "Downloading logstash config"
-wget https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/01-output-file.conf
-mv 01-output-file.conf /etc/logstash/conf.d/
+echo "Downloading logstash output config"
+wget https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/01-output.conf
+mv 01-output.conf /etc/logstash/conf.d/
 
 # Install iptables services (Firewalld akan dinonaktifkan)
 echo "Disabling firewalld and installing iptables-services"
 systemctl stop firewalld
 systemctl disable firewalld
 yum install iptables-services -y
+echo "Buka port 4369,25672,5671,5672,15672,61613,61614,1883,8883,5555,16384,9200,9300"
 echo "Silahkan sesuaikan rules iptables dan start iptables"
--- script/install-logstash.sh
+++ script/install-logstash.sh
@@ -3,4 +3,3 @@ wget https://raw.githubusercontent.com/akhfa/ta/master/repo/logstash.repo
 mv -f logstash.repo /etc/yum.repos.d/
 rpm --import https://packages.elastic.co/GPG-KEY-elasticsearch
 yum -y install logstash
-sed -i '/#LS_USER=logstash/a LS_USER=root' /etc/sysconfig/logstash
