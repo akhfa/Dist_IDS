5806e59ed2b02a1dbdfa601a73eeb9b584827c60
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-07-26 07:38:51 +0700

    add_exchange_queue update param

--- script/add_exchange_queue.sh
+++ script/add_exchange_queue.sh
@@ -2,7 +2,7 @@
 # 	Digunakan untuk membuat exchange dan queue
 
 # Format: 
-#	./add_exchange_queue.sh <rabbitmq-host> <vhost> <user> <password> <exchange>
+#	./add_exchange_queue.sh <rabbitmq-host> <vhost> <user> <password> <exchange> <exchange_type> <queue>
 
 # Keterangan:
 #       host            = host dari rabbitmq
@@ -10,6 +10,8 @@
 #       user            = user rabbitmq
 #       password        = password dari user rabbitmq
 #       exchange        = cluster
+#		exchange_type	= direct / fanout
+#		queue 			= Queue
 
 #!/bin/sh
 # assign variable from user input
@@ -19,7 +21,7 @@ user=$3
 password=$4
 exchange=$5
 exchange_type=$6
-queue=$(hostname)
+queue=7
 
 # add exchange dan queue pada server rabbitmq
 rabbitmqadmin -V $vhost -u $user -p $password -H $host declare exchange name=$exchange type=$exchange_type
