d8f9a8a90d02a926fb2678963f1cd068c22b4907
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-08-09 15:50:27 +0700

    install beaver: fixing

--- script/install-beaver.sh
+++ script/install-beaver.sh
@@ -1,22 +1,22 @@
 #!/bin/sh
 
-if($1 == ""); then
+if [$1 == ""]; then
 	echo "rabbitmq host:"
-	input -e rabbitmq-host
+	input -e rabbitmqHost
 	echo "rabbitmq vhost:"
-	input -e rabbitmq-vhost
+	input -e rabbitmqVhost
 	echo "rabbitmq username:"
-	input -e rabbitmq-username
+	input -e rabbitmqUsername
 	echo "rabbitmq password:"
-	input -e rabbitmq-password
+	input -e rabbitmqPassword
 	echo "rabbitmq exchange:"
-	input -e rabbitmq-exchange
+	input -e rabbitmqExchange
 else
-	rabbitmq-host = $1
-	rabbitmq-vhost = $2
-	rabbitmq-username = $3
-	rabbitmq-password = $4
-	rabbitmq-exchange = $5
+	rabbitmqHost = $1
+	rabbitmqVhost = $2
+	rabbitmqUsername = $3
+	rabbitmqPassword = $4
+	rabbitmqExchange = $5
 fi
 
 wget https://bootstrap.pypa.io/get-pip.py
@@ -26,8 +26,8 @@ mkdir /etc/beaver
 
 wget -O https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/beaver.conf
 
-sed -i "s/<rabbitmq-host>/$rabbitmq-host/" beaver.conf
-sed -i "s/<rabbitmq-vhost>/$rabbitmq-vhost/" beaver.conf
-sed -i "s/<rabbitmq-username>/$rabbitmq-username/" beaver.conf
-sed -i "s/<rabbitmq-password>/$rabbitmq-password/" beaver.conf
-sed -i "s/<rabbitmq-exchange>/$rabbitmq-exchange/" beaver.conf
+sed -i "s/<rabbitmqHost>/$rabbitmq-host/" beaver.conf
+sed -i "s/<rabbitmqVhost>/$rabbitmq-vhost/" beaver.conf
+sed -i "s/<rabbitmqUsername>/$rabbitmq-username/" beaver.conf
+sed -i "s/<rabbitmqPassword>/$rabbitmq-password/" beaver.conf
+sed -i "s/<rabbitmqExchange>/$rabbitmq-exchange/" beaver.conf
