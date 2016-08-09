6c71148f8751a80d8461303d3210cc21abdd3c1f
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-08-09 15:52:03 +0700

    install beaver: fixing

--- script/install-beaver.sh
+++ script/install-beaver.sh
@@ -1,22 +1,17 @@
 #!/bin/sh
 
 if [$1 == ""]; then
-	echo "rabbitmq host:"
-	input -e rabbitmqHost
-	echo "rabbitmq vhost:"
-	input -e rabbitmqVhost
-	echo "rabbitmq username:"
-	input -e rabbitmqUsername
-	echo "rabbitmq password:"
-	input -e rabbitmqPassword
-	echo "rabbitmq exchange:"
-	input -e rabbitmqExchange
+	read -p "rabbitmq host: " -e rabbitmqHost
+	read -p "rabbitmq vhost:" -e rabbitmqVhost
+	read -p "rabbitmq username:" -e rabbitmqUsername
+	read -p "rabbitmq password:" -e rabbitmqPassword
+	read -p "rabbitmq exchange:" -e rabbitmqExchange
 else
-	rabbitmqHost = $1
-	rabbitmqVhost = $2
-	rabbitmqUsername = $3
-	rabbitmqPassword = $4
-	rabbitmqExchange = $5
+	rabbitmqHost=$1
+	rabbitmqVhost=$2
+	rabbitmqUsername=$3
+	rabbitmqPassword=$4
+	rabbitmqExchange=$5
 fi
 
 wget https://bootstrap.pypa.io/get-pip.py
@@ -24,10 +19,12 @@ python get-pip.py
 pip install beaver==36.2.0
 mkdir /etc/beaver
 
-wget -O https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/beaver.conf
+wget https://raw.githubusercontent.com/akhfa/ta/master/config/cluster/beaver.conf
 
-sed -i "s/<rabbitmqHost>/$rabbitmq-host/" beaver.conf
-sed -i "s/<rabbitmqVhost>/$rabbitmq-vhost/" beaver.conf
-sed -i "s/<rabbitmqUsername>/$rabbitmq-username/" beaver.conf
-sed -i "s/<rabbitmqPassword>/$rabbitmq-password/" beaver.conf
-sed -i "s/<rabbitmqExchange>/$rabbitmq-exchange/" beaver.conf
+sed -i "s/<rabbitmq-host>/$rabbitmqHost/" beaver.conf
+sed -i "s/<rabbitmq-vhost>/$rabbitmqVhost/" beaver.conf
+sed -i "s/<rabbitmq-username>/$rabbitmqUsername/" beaver.conf
+sed -i "s/<rabbitmq-password>/$rabbitmqPassword/" beaver.conf
+sed -i "s/<rabbitmq-exchange>/$rabbitmqExchange/" beaver.conf
+
+mv beaver.conf /etc/beaver/
