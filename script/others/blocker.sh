fa59e04a03ac127cf931e74dd10e8af1c758a92b
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-09-21 01:29:13 +0700

    script: blocker.sh change from iptables -A to -I

--- script/others/blocker.sh
+++ script/others/blocker.sh
@@ -5,7 +5,7 @@ IP=$1
 SECONDS=$2
 OUTPUT=$(iptables -nL | grep "$IP" | grep "DROP" | grep all)
 if [ -z "${OUTPUT}" ]; then # jika belum ada rules nya
-	iptables -A INPUT -s $IP -j DROP
+	iptables -I INPUT 1 -s $IP -j DROP
 	echo "$IP blocked"
 
 	sleep ${SECONDS}
