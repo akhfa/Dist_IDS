a8ca05560007666a1f78cea5797a6c5e7fb513b3
Akhmad Fakhoni Listiyan Dede <akhmadfakhoni@gmail.com>
2016-09-20 23:56:24 +0700

    added blocker script

--- script/others/blocker.sh
+++ script/others/blocker.sh
@@ -2,8 +2,14 @@
 
 #!/bin/bash
 IP=$1
+SECONDS=$2
 OUTPUT=$(iptables -nL | grep "$IP" | grep "DROP" | grep all)
 if [ -z "${OUTPUT}" ]; then # jika belum ada rules nya
 	iptables -A INPUT -s $IP -j DROP
-	echo "rule added"
+	echo "$IP blocked"
+
+	sleep ${SECONDS}
+
+	iptables -D INPUT -s $IP -j DROP
+	echo "$IP unblocked"
 fi
